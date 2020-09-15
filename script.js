2f7b8946f17ae8489d91771eaef8d3d9bba03e66
yinzhema <mapb@bc.edu>
2020-09-15 12:35:37 -0400

    version 1.3 with local json file

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const getAttractions= () =>{
 		let error=false;
 		if(!error)		
 			resolve(
-				fetch("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/9d7f28c1-b3cb-41cc-b1ab-c42f5b62beeb/attractions.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200914%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200914T021335Z&X-Amz-Expires=86400&X-Amz-Signature=82c1cd9406b79cf16181084c37954bcc3acb2a18fe56824dfb240d5bc1cf409d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22attractions.json%22")
+				fetch("attractions.json")
 				.then(response => response.json())
 				.then(data =>{
 					attractions=data;
@@ -24,19 +24,6 @@ function filterData(category) {
 	} 
 	let sorted_attractions=filtered_attractions.sort((a1,a2)=>{a2['Visitors']-a1['Visitors']}).slice(0,5);
 	renderBarChart(sorted_attractions);
-	/* **************************************************
-	 *
-	 * TODO: filter attractions by the selected category
-	 * TODO: filter top 5 attractions
-	 *
-	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-	 *
-	 * renderBarChart(data)
-	 *
-	 * - 'data' must be an array of JSON objects
-	 * - the max. length of 'data' is 5
-	 *
-	 * **************************************************/
 
 }
 
